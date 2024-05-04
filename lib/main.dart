6383c0c954a5e73f548a182ea20ce8db7b84fa8e
Jehudavd <jehudavd@gmail.com>
2024-05-04 16:12:37 +0700

    initialize commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_course/form_page.dart';
+import 'package:flutter_app_course/login_page.dart';
+import 'package:flutter_app_course/product_page.dart';
+
+void main() {
+  runApp(const ApplicationFirst());
+}
+
+class ApplicationFirst extends StatelessWidget {
+  const ApplicationFirst({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: ProductPage()
+      home: LoginPage(),
+      // home: FormPage(),
+    );
+  }
+}
---
+++ lib/product_page.dart
@@ -0,0 +1,102 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app_course/form_page.dart';
+
+class ProductPage extends StatelessWidget {
+  String name;
+  ProductPage({super.key, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+          endDrawer: navDrawer(context),
+          appBar: AppBar(
+            leading: BackButton(
+              onPressed: () => Navigator.pop(context),
+            ),
+            title: Text('Daftar Product'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                child: Text('Hello, $name'),
+              ),
+              Container(
+                height: MediaQuery.sizeOf(context).height / 7,
+                child: ListView.builder(
+                  itemCount: 5,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => Card(
+                    elevation: 2,
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        side: BorderSide(color: Colors.black)),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 5),
+                          child: Image.network(
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJHB2LmJDE8mRo5vCggGcP-G5Jkov0nOYt700GGxzzQg&s',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Name Product'),
+                            Text('15.000'),
+                            Text('5 pcs')
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+
+  Widget navDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          header(),
+          dataItem(context, Icon(Icons.home), 'Home', 'Halaman pertama dibuka'),
+          dataItem(context, Icon(Icons.book), 'Book', 'Halaman daftar buku')
+        ],
+      ),
+    );
+  }
+
+  Widget header() {
+    return UserAccountsDrawerHeader(
+      decoration: BoxDecoration(color: Colors.blue),
+      accountName: Text(name),
+      accountEmail: Text('$name@gmail.com'),
+      currentAccountPicture: ClipOval(
+        child: Image.network(
+            'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'),
+      ),
+    );
+  }
+
+  Widget dataItem(BuildContext context, Icon icon, String valueTitle,
+      String valueSubtitle) {
+    return ListTile(
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => FormPage(),
+          )),
+      leading: icon,
+      title: Text(valueTitle),
+      subtitle: Text(valueSubtitle),
+    );
+  }
+}
